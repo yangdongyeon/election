6dfede42b71217bda8dccb408846aaf7e5120729
yangdongyeon <cori1219@kaist.ac.kr>
2024-04-22 16:39:24 +0900

    ready for web hosting

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-bv1kc=ztx2xu)(ebvmj@w)g4+l3iyarne(*33=*#(b(6p=oyl='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['kaistudc.pythonanywhere.com','*']
 
 
 # Application definition
